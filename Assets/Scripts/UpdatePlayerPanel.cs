25a4d200c7eeabea7e4bad02a6896f86fc2159fc
Ann Ho <kjuri27@yahoo.com>
2016-03-14 17:47:46 -0400

    There is now a highlight on the company you're buying/selling

---
+++ Assets/Scripts/UpdatePlayerPanel.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UpdatePlayerPanel : MonoBehaviour {
+
+	public Image highlight;
+
+	[Header("Text Objects")]
+	public Text CurrentMoney;
+	public Text[] CompanyShares;
+
+	void Update () {
+		updateSelectedCom (GetComponent<Player> ().selectedCompany);
+
+		for (CompanyName i = 0; i != CompanyName.size; i++) {
+			updateShare (i, GetComponent<Player> ().CompanyShares [(int)i]);
+		}
+		updateMoney(GetComponent<Player>().currentMoney);
+	}
+
+	void updateMoney(float newValue) {
+		string finalValue = string.Format ("{0:C}", newValue);
+		finalValue = finalValue.Insert (1, " ");
+		CurrentMoney.text = finalValue;
+	}
+
+	void updateShare(CompanyName whichCom, int newValue) {
+		string finalValue = newValue.ToString () + "k";
+		CompanyShares [(int)whichCom].text = finalValue;
+	}
+
+	void updateSelectedCom(CompanyName whichCom) {
+
+		if (whichCom == CompanyName.none) {
+			highlight.enabled = false;
+		} 
+		else {
+			highlight.enabled = true;
+		}
+
+		if (whichCom == CompanyName.A) {
+			highlight.transform.localPosition = new Vector3 (7f, -30f, 0);
+		} 
+		else if (whichCom == CompanyName.B) {
+			highlight.transform.localPosition = new Vector3 (67f, -30f, 0);
+		} 
+		else if (whichCom == CompanyName.C) {
+			highlight.transform.localPosition = new Vector3 (128f, -30f, 0);
+		} 
+	}
+}
