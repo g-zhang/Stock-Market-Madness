aa7f565cd929cef153daca7f4265b84497720dc6
Jonathan Smith <jnthsmth@umich.edu>
2016-03-19 16:53:49 -0400

    added stockevent, stockprice, and model classes

---
+++ Assets/Scripts/Model.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+public class Model {
+
+	public StockEvent[] availableEvents;
+	public Queue<StockEvent> eventQueue;
+
+	public void Tick() {
+
+	}
+
+	public bool Buy(string player, string stock, int number) {
+		return true;
+	}
+
+	public bool Sell(string player, string stock, int number) {
+		return true;
+	}
+
+	public void ActivateEvent(StockEvent stockEvent) {
+		return;
+	}
+
+}
---
+++ Assets/Scripts/StockEvent.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StockEvent {
+	string eventName;
+	string tickerText;
+	// Will contain information on how the stocks are affected
+}
