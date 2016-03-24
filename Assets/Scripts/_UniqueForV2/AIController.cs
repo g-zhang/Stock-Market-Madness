c497bb98dacbe62bd0229b225848559bb2774431
Ann Ho <kjuri27@yahoo.com>
2016-03-24 15:55:09 -0400

    Very Basic AI for Crowd

---
+++ Assets/Scripts/_UniqueForV2/AIController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIController : MonoBehaviour {
+
+	public enum AIBehavior {lowestBuyer = 0, size};
+
+	public AIBehavior Behavior;
+	public GameObject[] CompanyBooth = new GameObject[(int)CompanyName.size];
+
+	NavMeshAgent agn;
+
+	// Use this for initialization
+	void Start () {
+		agn = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Behavior == AIBehavior.lowestBuyer) {
+			agn.destination = CompanyBooth[(int) findLowest()].transform.position;
+		}
+	}
+
+	CompanyName findLowest() {
+		float lowestPrice = Model.Instance.stocks [(int)CompanyName.A].Price;
+		CompanyName lowestPriceCom = CompanyName.A;
+		for (CompanyName i = CompanyName.B; i != CompanyName.size; i++) {
+			if (lowestPrice > Model.Instance.stocks [(int)i].Price) {
+				lowestPrice = Model.Instance.stocks [(int)i].Price;
+				lowestPriceCom = i;
+			}
+		}
+		return lowestPriceCom;
+	}
+}
